61f77aa8c3a94a8356ce7ec5db3d1fa6e5303e58
Dmytro Tsysar <1106994+idmytro@users.noreply.github.com>
2025-02-26 14:36:59 +0100

    build: add eslint, run autofix

--- svelte.config.js
+++ svelte.config.js
@@ -1,28 +1,28 @@
-import adapter from '@sveltejs/adapter-cloudflare';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-cloudflare'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+  // Consult https://svelte.dev/docs/kit/integrations
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
 
-	kit: {
-		adapter: adapter({
+  kit: {
+    adapter: adapter({
       pages: true,
-			// See below for an explanation of these options
-			routes: {
-				include: ['/*'],
-				exclude: ['<all>']
-			},
-			platformProxy: {
-				configPath: 'wrangler.toml',
-				environment: undefined,
-				experimentalJsonConfig: false,
-				persist: false
-			}
-		})
-	}
-};
+      // See below for an explanation of these options
+      routes: {
+        include: ['/*'],
+        exclude: ['<all>'],
+      },
+      platformProxy: {
+        configPath: 'wrangler.toml',
+        environment: undefined,
+        experimentalJsonConfig: false,
+        persist: false,
+      },
+    }),
+  },
+}
 
-export default config;
+export default config
