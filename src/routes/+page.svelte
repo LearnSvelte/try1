0e24d9c6900ad6c21a0c258198df486c41daa33f
Dmytro Tsysar <1106994+idmytro@users.noreply.github.com>
2025-02-26 21:14:58 +0100

    feat: bind vars

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,24 @@
-<form>
-  <input type="text" name="long" placeholder="Long URL" />
-  <input type="text" name="short" placeholder="Short URL" />
-  <button type="submit">Submit</button>
+<script lang="ts">
+  let longUrl: string = ''
+  let shortUrl: string = ''
 
-  <p>
-    <a href="/shortUrl">Go to short url</a>
-  </p>
-  <p>
-    <a href="/shortUrl/stats">Go to stats</a>
-  </p>
+  function handleSubmit(event: Event) {
+    event.preventDefault()
+    console.log('Long URL:', longUrl)
+    console.log('Short URL:', shortUrl)
+  // Later you would use your model here to save the URL mapping
+  }
+</script>
+
+<form on:submit={handleSubmit}>
+  <input type="text" name="longurl" placeholder="Long URL" bind:value={longUrl} />
+  <input type="text" name="shorturl" placeholder="Short URL" bind:value={shortUrl} />
+  <button type="submit">Submit</button>
 </form>
+
+<p>
+  <a href="/{shortUrl}">Go to short url</a>
+</p>
+<p>
+  <a href="/{shortUrl}/stats">Go to stats</a>
+</p>
