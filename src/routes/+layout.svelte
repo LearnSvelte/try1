f0de20492109448258072df25914aaa7c78c3ce9
Dmytro Tsysar <1106994+idmytro@users.noreply.github.com>
2025-02-26 15:17:38 +0100

    feat: layout, wrangler, editorconfig

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+  // export let data;
+</script>
+
+<main>
+  <header>
+    <h1>Мій сайт</h1>
+    <nav>
+      <a href="/">Головна</a>
+      <a href="/about">Про нас</a>
+    </nav>
+  </header>
+
+  <slot />
+
+  <footer>
+    <p>© 2025, Всі права захищено</p>
+  </footer>
+</main>
