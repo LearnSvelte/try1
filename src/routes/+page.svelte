0fe7ef6a12d45ba408f6a80dd482215c7c2b244d
Dmytro Tsysar <1106994+idmytro@users.noreply.github.com>
2025-02-27 22:40:33 +0100

    feat: validating wip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,13 +4,35 @@
   import { catchError } from '$lib/catchError'
   import { saveURL } from '$lib/saveURL'
 
-  let url: string = ''
-  let slug: string = ''
-  let error: string = ''
-  let submitState: 'idle' | 'submitting' | 'success' | 'error' = 'idle'
+  let url: string = $state('')
+  let myUrl = $state(page.url)
+  let slug: string = $state('')
+  let error: string = $state('')
+  let submitState: 'idle' | 'validating' | 'submitting' | 'success' | 'error' = $state('idle')
+  let urlIsValid = $state(false)
+  let slugIsValid = $state(false)
+
+  function validateUrl () {
+    console.log('Validating URL:', url)
+    try {
+      const _newUrl = new URL(url)
+      urlIsValid = true
+    }
+    catch (_err: unknown) {
+      urlIsValid = false
+    }
+  }
 
   async function handleSubmit (event: Event) {
+    console.log('Submitting form...')
+    submitState = 'validating'
     event.preventDefault()
+
+    validateUrl()
+
+    if (!urlIsValid)
+      return
+
     console.log('Long URL:', url)
     console.log('Short URL:', slug)
 
@@ -29,8 +51,25 @@
     console.log('Data:', data)
   }
 
-  function handleReset () {
+  function handleReset (event: Event) {
     submitState = 'idle'
+
+    setTimeout(() => {
+      myUrl = page.url
+    })
+  }
+
+  function handleUrlInput (event: Event) {
+    console.log('URL changed:', url)
+    if (submitState === 'idle')
+      return
+    validateUrl()
+  }
+
+  function getIsUrlValid () {
+    if (submitState === 'idle')
+      return null
+    return urlIsValid ? 'false' : 'true'
   }
 </script>
 
@@ -48,15 +87,17 @@
     required
     title="Please enter a valid URL starting with http:// or https://"
     autocomplete="off"
+    aria-invalid={getIsUrlValid()}
     style="text-align: center"
+    on:input={handleUrlInput}
   />
 
   <!-- svelte-ignore a11y_no_redundant_roles -->
   <fieldset role="group">
     <input
       type="text"
-      disabled
-      bind:value={page.url}
+      bind:value={myUrl}
+      readonly
       style="text-align: right"
     />
     <input
@@ -73,6 +114,7 @@
   <button
     type="submit"
     disabled={submitState !== 'idle'}
+    on:click={handleSubmit}
   >
     Submit
   </button>
