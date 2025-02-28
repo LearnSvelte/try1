64073c05448c94e446950b6af5d25bfad3164d79
Dmytro Tsysar <1106994+idmytro@users.noreply.github.com>
2025-02-28 16:41:45 +0100

    chore: comms

--- src/routes/[slug]/stats/+page.svelte
+++ src/routes/[slug]/stats/+page.svelte
@@ -5,16 +5,10 @@
   const { data } = $props();
 
   const pageUrl = page.url
-  // let redirectUrl = page.url.split('/').slice(0, -1).join('/');
-
-  // let redirectUrl = $derived(page.url, (u) => {
-  //   return u.split('/').slice(0, -1).join('/');
-  // });
 
   let shortUrl = $derived(pageUrl.toString().split('/').slice(0, -1).join('/'));
 </script>
 
-<!-- <p>{pageUrl}</p> -->
 <p>
   <a href={shortUrl}>{shortUrl}</a> 👉
   {data.url ?? 'url is missing'}</p>
