57571881fd9a7d2479d8ae89b1c31421cdb4dd34
Dmytro Tsysar <1106994+idmytro@users.noreply.github.com>
2025-03-02 19:56:52 +0100

    feat: initial files

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,62 @@
+/* eslint perfectionist/sort-objects: 2 */
 import antfu from '@antfu/eslint-config'
 
-export default antfu({
-  svelte: true,
-})
+export default antfu(
+  {
+    svelte: true,
+  },
+  {
+    rules: {
+      'antfu/if-newline': 0,
+      'prefer-const': 0,
+      'prefer-template': 2,
+      'style/object-curly-newline': [
+        2,
+        {
+          ExportDeclaration: {
+            minProperties: 2,
+            multiline: true,
+          },
+        },
+      ],
+      'style/space-before-function-paren': [
+        2,
+        {
+          anonymous: 'always',
+          asyncArrow: 'always',
+          named: 'always',
+        },
+      ],
+    },
+  },
+  /* svelte */
+  {
+    rules: {
+      'svelte/html-quotes': [2, {
+        dynamic: {
+          avoidInvalidUnquotedInHTML: false,
+          quoted: false,
+        },
+        prefer: 'double',
+      }],
+    },
+  },
+  /* vanilla-ts pairs */
+  {
+    rules: {
+      'no-unused-vars': 0,
+      'ts/no-unused-vars': [
+        2,
+        {
+          args: 'all',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+          varsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
+)
--- svelte.config.js
+++ svelte.config.js
@@ -3,23 +3,18 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://svelte.dev/docs/kit/integrations
-  // for more information about preprocessors
   preprocess: vitePreprocess(),
 
   kit: {
     adapter: adapter({
       pages: true,
-      // See below for an explanation of these options
       routes: {
         include: ['/*'],
         exclude: ['<all>'],
       },
       platformProxy: {
-        configPath: 'wrangler.toml',
-        environment: undefined,
-        experimentalJsonConfig: false,
-        persist: false,
+        configPath: 'wrangler.jsonc',
+        persist: './worker-local',
       },
     }),
   },
