65ff8739629ead193809fb00cdffa39c8388a9ec
Dmytro Tsysar <1106994+idmytro@users.noreply.github.com>
2025-03-02 17:12:02 +0100

    refactor: clean api/saveStats

--- src/routes/api/saveStats/+server.ts
+++ src/routes/api/saveStats/+server.ts
@@ -6,20 +6,16 @@ import {
   isStatValid,
 } from '$lib/entities/stat'
 import { getKVOrErrorRes } from '$lib/server'
-import { isNonEmptyString } from '$lib/shared'
+import { isNonEmptyString, unknownErrorResponse } from '$lib/shared'
 import { json } from '@sveltejs/kit'
 
 export const POST: RequestHandler = async ({ request, platform }) => {
-  const [res, kv] = getKVOrErrorRes(platform)
-  if (res)
-    return res
+  const [kvErrorRes, kv] = getKVOrErrorRes(platform)
+  if (kvErrorRes)
+    return kvErrorRes
 
   try {
-    // const { slug, data } = await request.json()
-
-    const { slug, stat } = (await request.json()) as { slug?: unknown, stat?: unknown }
-    // if (!slug || !url || isStatsValid(stat))
-    //   return json({ error: 'Missing slug, url or stat' }, { status: 400 })
+    const { slug, stat }: { slug?: unknown, stat?: unknown } = await request.json()
 
     if (!isStatValid(stat) || !isNonEmptyString(slug))
       return json({ error: 'Invalid stat, or missing slug' }, { status: 400 })
@@ -37,64 +33,6 @@ export const POST: RequestHandler = async ({ request, platform }) => {
     return new Response(`Saved ${kvKeyStats}`, { status: 200 })
   }
   catch (error) {
-    console.error('❌ KV put error:', error)
-    return new Response(`❌ Error: ${error instanceof Error ? error.message : 'Unknown error'}`, { status: 500 })
+    return unknownErrorResponse(error)
   }
 }
-
-// export const GET: RequestHandler = async ({ request, platform }) => {
-//   if (!platform || !platform.env) {
-//     return new Response(JSON.stringify({ error: 'Platform env is undefined' }), {
-//       status: 500,
-//       headers: { 'Content-Type': 'application/json' },
-//     })
-//   }
-
-//   const kv = platform.env.BINDING_NAME
-
-//   if (!kv) {
-//     return new Response(JSON.stringify({ error: 'KV binding is missing' }), {
-//       status: 500,
-//       headers: { 'Content-Type': 'application/json' },
-//     })
-//   }
-
-//   try {
-//     const url = new URL(request.url)
-//     const slug = url.searchParams.get('slug')
-
-//     if (!slug) {
-//       return new Response(JSON.stringify({ error: 'Missing slug parameter' }), {
-//         status: 400,
-//         headers: { 'Content-Type': 'application/json' },
-//       })
-//     }
-
-//     const key = `${CLICK_PREFIX}${slug}`
-
-//     const value = await kv.get(key)
-
-//     console.log('////////////value:', value)
-
-//     return new Response(
-//       JSON.stringify({
-//         exists: value !== null,
-//         value,
-//       }),
-//       {
-//         status: 200,
-//         headers: { 'Content-Type': 'application/json' },
-//       },
-//     )
-//   }
-//   catch (error) {
-//     console.error('❌ KV get error:', error)
-//     return new Response(
-//       JSON.stringify({ error: error instanceof Error ? error.message : 'Unknown error' }),
-//       {
-//         status: 500,
-//         headers: { 'Content-Type': 'application/json' },
-//       },
-//     )
-//   }
-// }
